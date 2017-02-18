d94e38a07058cab4afa5673f83df916787be0797
illuzor <illuzor@gmail.com>
2017-02-19 01:04:33 +0300

    missing semicolons added

--- source/action-play.js
+++ source/action-play.js
@@ -14,8 +14,8 @@ if (player.offsetWidth && player.offsetHeight) {
         var divs = document.querySelectorAll('.played_status_1, .played_status_2');
 
         if (divs.length) {
-            var num = 0
-            if (play == "random")
+            var num = 0;
+            if (play == "random");
                 num = Math.floor(Math.random() * divs.length);
             divs[num].getElementsByClassName("episode_button")[0].click();
             result = "play";
