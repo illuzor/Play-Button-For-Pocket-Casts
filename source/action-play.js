c1f2d0848426c5fe6f2f36fa4d0e4a2128877433
illuzor <illuzor@gmail.com>
2017-05-03 16:27:26 +0300

    first podcast from bottom option (closes #10)

--- source/action-play.js
+++ source/action-play.js
@@ -14,9 +14,20 @@ if (player.offsetWidth && player.offsetHeight) {
         var divs = document.querySelectorAll('.played_status_1, .played_status_2, .played_status_');
 
         if (divs.length) {
-            var num = 0;
-            if (play == "random")
-                num = Math.floor(Math.random() * divs.length);
+            var num;
+			
+			switch (play) {
+				case "first":
+					num = 0
+					break;
+				case "last":
+					num = divs.length - 1
+					break;
+				case "random":
+					num = Math.floor(Math.random() * divs.length);
+					break;
+			}
+			
             divs[num].getElementsByClassName("episode_button")[0].click();
             result = "play";
         }
