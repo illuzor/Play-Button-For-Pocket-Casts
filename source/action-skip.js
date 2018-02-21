4fa582774ce0f1c5c16f8c71efd962fd0c817349
Nicholas Buse <NicholasBuse@users.noreply.github.com>
2018-02-21 14:09:52 -0600

    Fixed typo

--- source/action-skip.js
+++ source/action-skip.js
@@ -1,2 +1,2 @@
-if (document.getElementsByClassName("animated-play-button").lenght != 0)
+if (document.getElementsByClassName("animated-play-button").length != 0)
     document.getElementsByClassName(type)[0].click();
