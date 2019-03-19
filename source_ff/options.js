0ddea93c59e90157ef4f4c2cedd05e60241cab0b
illuzor <illuzor@gmail.com>
2019-03-19 22:21:44 +0300

    shortcuts for firefox added (closes #18)

--- source_ff/options.js
+++ source_ff/options.js
@@ -1,4 +1,5 @@
-document.getElementById('ntp_enabled_label').style.visibility = "hidden";
+document.getElementById('play_enabled_label').innerHTML += chrome.i18n.getMessage('enable_play_pause_s');
+document.getElementById('skip_enabled_label').innerHTML += chrome.i18n.getMessage('enable_play_prev_next_s');
 document.getElementById('ntp_enabled_label').innerHTML += chrome.i18n.getMessage('show_ntp');
 document.getElementById('page_label').innerHTML = chrome.i18n.getMessage('page_to_open') + ": " + document.getElementById('page_label').innerHTML;
 document.getElementById('pin_tab_label').innerHTML += chrome.i18n.getMessage('pin_tab');
@@ -18,10 +19,12 @@ document.getElementById('play').options[3].text = chrome.i18n.getMessage('play_n
 
 function saveOptions() {
     chrome.storage.sync.set({
+        play_enabled: document.getElementById('play_enabled').checked,
+        skip_enabled: document.getElementById('skip_enabled').checked,
         ntp_enabled: document.getElementById('ntp_enabled').checked,
+        pin_tab: document.getElementById('pin_tab').checked,
         play: document.getElementById('play').value,
-        page: document.getElementById('page').value,
-        pin_tab: document.getElementById('pin_tab').checked
+        page: document.getElementById('page').value
     }, function() {
         window.close();
     });
@@ -29,16 +32,20 @@ function saveOptions() {
 
 function restoreOptions() {
     chrome.storage.sync.get({
+            play_enabled: true,
+            skip_enabled: true,
             ntp_enabled: true,
+            pin_tab: false,
             play: "first",
-            page: "default",
-            pin_tab: false
+            page: "default"
         },
         function(items) {
+            document.getElementById('play_enabled').checked = items.play_enabled;
+            document.getElementById('skip_enabled').checked = items.skip_enabled;
             document.getElementById('ntp_enabled').checked = items.ntp_enabled;
+            document.getElementById('pin_tab').checked = items.pin_tab;
             document.getElementById('play').value = items.play;
             document.getElementById('page').value = items.page;
-            document.getElementById('pin_tab').checked = items.pin_tab;
         });
 }
 
