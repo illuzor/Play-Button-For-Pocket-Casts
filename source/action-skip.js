9cb26cfacca67babd8268416de40b7f4abec7719
Artem Zanin <illuzor@gmail.com>
2023-02-24 12:43:28 +0600

    minor refactor

--- source/action-skip.js
+++ source/action-skip.js
@@ -1,5 +1,5 @@
 function performSkip(type) {
-    var buttons = document.getElementsByClassName(type);
+    let buttons = document.getElementsByClassName(type);
     if (buttons.length) {
         buttons[0].click();
     }
--- source/log-listener.js
+++ source/log-listener.js
@@ -3,19 +3,23 @@ if (typeof listenLog === 'undefined') {
     const EXT_ID = "ogdnlmiknnmedpcnjnkjncdjjgfdkiik"
     const EXT_ID_LOCAL = "gimcijegdcaeebbegnkglpgmpgmkeklo"
 
+    let extensionIds = [EXT_ID, EXT_ID_LOCAL]
+
     window.addEventListener("LogEvent", function (e) {
         var event = e.detail;
         if (event.includes("[Audio] playing") || event.includes("[Audio] waiting")) {
-            chrome.runtime.sendMessage(EXT_ID, {state: "Pause"});
-            chrome.runtime.sendMessage(EXT_ID_LOCAL, {state: "Pause"});
+            extensionIds.forEach(id =>
+                chrome.runtime.sendMessage(id, {state: "Pause"})
+            )
         } else if (event.includes("[Audio] pause") || event.includes("[Audio] ended") || event.includes("[Audio] abort")) {
-            chrome.runtime.sendMessage(EXT_ID, {state: "Play"});
-            chrome.runtime.sendMessage(EXT_ID_LOCAL, {state: "Play"});
+            extensionIds.forEach(id =>
+                chrome.runtime.sendMessage(id, {state: "Play"})
+            )
         }
     }, false);
 
     function listenLog() {
-        var originalLog = console.log;
+        let originalLog = console.log;
         console.log = function (e) {
             originalLog(e);
             window.dispatchEvent(new CustomEvent("LogEvent", {detail: e}));
